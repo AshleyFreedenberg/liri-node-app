154e44a1d8345f3db9d622a9f4a82daf7def731b
Yadi <ashleyfreedenberg@gmail.com>
2019-03-18 14:51:58 -0700

    work 3/18 2:50pm

--- liri.js
+++ liri.js
@@ -1,3 +1,12 @@
 var Spotify = require('node-spotify-api');
 
 var axios = require("axios");
+
+require('dotenv').config()
+
+var moment = require('moment');
+
+var keys = require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
+
