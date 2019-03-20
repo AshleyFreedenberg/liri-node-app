083e546361e532d7d3149fdffd16541811af6b73
Yadi <ashleyfreedenberg@gmail.com>
2019-03-20 11:04:08 -0700

    work from 3/20 11am

--- liri.js
+++ liri.js
@@ -36,15 +36,15 @@ function concert() {
     axios
         .get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
         .then(function (response) {
-            // If the axios was successful...
-            // Then log the body from the site!
-            console.log(response);
-            // (JSON.stringify(result, null, 2));
-            // console.log("Venue: " + response.data.venue);
-            // console.log("Location: " + response.data.venue.city);
-            // console.log("Date of Event: " + response.data.datetime);
-
-            //Date of the Event (use moment to format this as "MM/DD/YYYY")
+            for (var i = 0; i < response.data.length; i++) {
+                //console.log(response);
+                console.log("Venue: " + response.data[i].venue.name);
+                console.log("Location: " + response.data[i].venue.city);
+                var eventDate = response.data[i].datetime;
+                var convertedDate = moment(eventDate);
+                console.log("Date of Event: " + (convertedDate.format("MM/DD/YYYY")));
+                console.log("--------------------------");
+            }
         })
 }
 
@@ -56,18 +56,19 @@ function spotify() {
 
     var spotify = new Spotify(keys.spotify);
 
-    spotify.search({ type: 'track', query: song }, function (err, data) {
+    spotify.search({ type: 'track', query: song, limit: 1}, function (err, data) {
 
         if (err) {
             return console.log('Error occurred: ' + err);
         }
 
-        console.log(data);
-        // (JSON.stringify(result, null, 2));
-        // console.log("Artists: " + response);
-        // console.log("Song's Name: " + response);
-        // console.log("Preview Link: " + response);
-        // console.log("Album: " + response);
+        //console.log(data);
+        console.log("--------------------------");
+        console.log("Artists: " + data.tracks.items[0].album.artists[0].name);
+        console.log("Song's Name: " + data.tracks.items[0].name);
+        console.log("Preview Link: " + data.tracks.items[0].preview_url);
+        console.log("Album: " + data.tracks.items[0].album.name);
+        console.log("--------------------------");
     });
 
 }
@@ -79,11 +80,21 @@ function movie() {
     var defaultMovie = "Mr. Nobody."
 
     axios
-        .get("https://www.omdbapi.com/?apikey=trilogy&t=" + movie)
+        .get("https://www.omdbapi.com/?apikey=trilogy&t=" + movie + "&y=&plot=short")
         .then(function (response) {
 
             console.log(response);
-            
+            console.log("--------------------------");
+            console.log("Title: " + response.data.Title);
+            console.log("Year: " + response.data.Year );
+            console.log("IMDB Rating: " + response.data.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+            console.log("Country where the movie was produced: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+            console.log("--------------------------");
+
         })
 
 }
